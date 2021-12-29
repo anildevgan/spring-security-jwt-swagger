9c64e71a83f53aad43ff938e6d33b1f9668c99f6
Anil Kumar Devgan <anil.devgan91@gmail.com>
2021-12-29 15:00:29 +0530

    Classpath Heroku issue

--- src/main/java/com/devgan/jwtdemo/utils/ApplicationUtils.java
+++ src/main/java/com/devgan/jwtdemo/utils/ApplicationUtils.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -18,8 +19,9 @@ public class ApplicationUtils {
     public <T> List<T> parseObjectList(Class<T> type, String filename) {
         JavaType javaType = mapper.getTypeFactory().constructCollectionType(List.class, type);
         try {
-            ClassPathResource resource = new ClassPathResource(filename);
-            return mapper.readValue(resource.getFile(), javaType);
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource(filename).getFile());
+            return mapper.readValue(file, javaType);
         } catch (IOException e) {
            throw new RuntimeException(e);
         }
